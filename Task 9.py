19b0c31efb106e1e0c525001ce320c9cf1690102
Kris-Stank <kristibutorina09@gmail.com>
2025-12-11 16:10:16 +0500

    lucky ticket

---
+++ Task 9.py
@@ -0,0 +1,14 @@
+ticket = input()
+sum1, sum2 = 0, 0
+part1 = ticket[:3]
+part2 = ticket[3:]
+
+for i in range(3):
+    sum1 += int(ticket[i])
+    sum2 += int(ticket[i+3])
+
+if len(ticket) == 6:
+    if sum1 == sum2:
+        print("YES")
+    else:
+        print("NO")
