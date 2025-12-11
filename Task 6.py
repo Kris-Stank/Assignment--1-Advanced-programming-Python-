275c14b88ae9996a572ecadd7877e6556c872f7e
Kris-Stank <kristibutorina09@gmail.com>
2025-12-11 15:16:58 +0500

    Simple calculatooor

---
+++ Task 6.py
@@ -0,0 +1,12 @@
+num1 = int(input("First number: "))
+sign = input("Operation: ")
+num2 = int(input("Second number: "))
+
+if sign == "+":
+    print(num1 + num2)
+elif sign == "*":
+    print(num1 * num2)
+elif sign == "-":
+    print(num1 - num2)
+elif sign == "/":
+    print(num1 / num2)
