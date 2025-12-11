bb265a9f007d0a94886716e74e8ac9e8145786d3
Kris-Stank <kristibutorina09@gmail.com>
2025-12-11 13:01:38 +0500

    Initial commit: Asignment 1 tasks

---
+++ Task 1.py
@@ -0,0 +1,8 @@
+print("Hello!")
+name, surname = input("What're your name and surname?\n").split()
+age = int(input("How old are you?\n"))
+phone_num = input("What's your phone number\n")
+
+print(f'Your name is {name} and surname is {surname}')
+print(f'Your age is {age}')
+print(f'Your phone number is {phone_num}')
